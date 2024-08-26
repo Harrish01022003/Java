b7ca676150636cae38b9d00a23bd19854b3c924c
Harrish A <81296332+Harrish01022003@users.noreply.github.com>
2024-08-26 20:33:24 +0530

    Add files via upload

---
+++ week2/OddNumber.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class OddNumber {
+	public static void main(String[] args) {
+		for (int i = 1; i < 20; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+
+			}
+
+		}
+
+	}
+}
+/*
+ * console  1 3 5 7 9 11 13 15 17 19
+ */
---
+++ week2/PositiveNegative.java
@@ -0,0 +1,20 @@
+package week2;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+
+		if(number > 0) {
+			System.out.println(number + " Is Positive Number");
+		}
+		else {
+			System.out.println(number + " Is Negative");
+		}
+	}
+
+}
+
+//Console   10 Is Positive Number
+
