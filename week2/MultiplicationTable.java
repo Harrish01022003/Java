9276910359bdd45478edbbcff0463e9793450bc4
Harrish A <81296332+Harrish01022003@users.noreply.github.com>
2024-08-27 17:02:23 +0530

    Add files via upload

---
+++ week2/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		int num = 5;
+		for(int i = 1;i <= 10;i++) {
+			int sum = i * num;
+			System.out.println(i + " * " + num + " * " + sum);
+		}
+	}
+
+}
