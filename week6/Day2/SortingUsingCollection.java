0bd16958e1f8a0feff7c6d45463f74904528a8d1
Harrish A <81296332+Harrish01022003@users.noreply.github.com>
2024-09-24 14:04:53 +0530

    Add files via upload

---
+++ week6/Day2/SortingUsingCollection.java
@@ -0,0 +1,26 @@
+package week6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortingUsingCollection {
+
+	public static void main(String[] args) {
+		List<String> values = new ArrayList<String>();
+		values.add("HCL");
+		values.add("Wipro");
+		values.add("Aspire system");
+		values.add("CTS");
+		
+		int size = values.size();
+		
+		Collections.sort(values);
+		System.out.println(values);
+
+
+		
+
+	}
+
+}
