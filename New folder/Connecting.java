8dbf3652665b8ccec5d1a90dc816cfcdd5aeb425
Harrish A <81296332+Harrish01022003@users.noreply.github.com>
2025-04-08 18:38:11 +0530

    Add files via upload

---
+++ New folder/Connecting.java
@@ -0,0 +1,12 @@
+package week3.day1;
+
+public class Connecting extends Concrete{
+	public static void main(String[] args) {
+		Connecting obj = new Connecting();
+		obj.connect();
+		obj.disconnect();
+		obj.executeUpdate();
+		
+	}
+
+}
---
+++ New folder/DatabseConnection.java
@@ -0,0 +1,9 @@
+package week3.day1;
+
+public interface DatabseConnection {
+	void  connect();
+	void disconnect();
+	void executeUpdate();
+
+
+}
